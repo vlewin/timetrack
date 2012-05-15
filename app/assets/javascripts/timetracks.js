c36a570b43b75e233ed56f6e6a0685bf3c5c2e9b
vlewin <vlewin@suse.de>
2012-05-15 17:46:32 +0200

    add now button to input fields show popover with time and duration

--- app/assets/javascripts/timetracks.js
+++ app/assets/javascripts/timetracks.js
@@ -1,18 +1,24 @@
 $(document).ready(function() {
   $('a.date-select').live('click', function(event) {
     event.preventDefault();
-    console.log("Clicked");
-
     var ajax = $.get("timetracks.js", "date="+$(this).data("date"), function() {})
-
     ajax.complete(function() {  $("#timetrack").html(ajax.responseText); });
     return false;
   })
-})
 
-$(document).ready(function() {
-  $('a.clock').live('click', function(e) {
-    e.preventDefault();
-    $('#timetrack-form').toggle();
+  $('#timetrack-form button.now').live('click', function(event) {
+    event.preventDefault();
+    var $input = $(this).parent().find('input[type=text]');
+    var time = new Date();
+    $input.val(time.getHours() + ':' + time.getMinutes())
   })
+
+  $('#calendar a.date-select').on('hover', function() {
+    $('#calendar a.date-select').popover({
+      trigger : 'hover',
+      delay: 100,
+      placement : 'top'
+    })
+  })
+
 })
