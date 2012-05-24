9c82e34f1945ed7eddd6d9ec072b5d5f04685b01
vlewin <vlewin@gmx.de>
2012-05-24 22:22:15 +0200

    add sign in and sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Timestamps::Application.routes.draw do
   devise_for :users
+    
   devise_scope :user do
     get "sign_in", :to => "devise/sessions#new"
     get "sign_out", :to => "devise/sessions#destroy"
   end
+  
   resources :timetracks
   root :to => 'timetracks#index'
 end
