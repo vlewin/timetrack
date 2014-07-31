8f5ec120da0b00aa21db3ca56cdb1880c1bb3cf2
Vladislav Lewin <vlewin@suse.de>
2014-07-31 11:08:23 +0200

    clean up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,8 +17,6 @@ module ActionView
   module Helpers
     module FormHelper
       def time_select(object_name, method, options = {}, html_options = {})
-        puts "**** #{options[:object].inspect}"
-
         object = options[:object]
         default_options = {
           start_hour: 6,
@@ -30,64 +28,13 @@ module ActionView
         options.merge!(default_options)
 
         if method == :finish
-          options[:start_hour] = object.start.hour || 10
-          options[:start_minute] = object.start.min || 00
+          options[:start_hour] = object.start.hour
+          options[:start_minute] = object.start.min
         end
 
-
-        puts "*** OPTIONS: #{options.inspect}"
         Tags::TimeSelect.new(object_name, method, self, options, html_options).render
       end
-
-      # def time_field(object, method, options = {})
-      #   byebug
-      #   object = options[:object]
-
-      #   time_select(method)
-
-
-      #   # content_tag(:div, text_field(object_name, method, options)
-      #   # + content_tag(:button, content_tag(:i, '', :class=>"fa fa-clock-o"), :class => "btn now"), :class=>"input-append")
-      # end
-
-      # def duration
-      #   if Timetrack.balance(current_user)[:negative]
-      #     css_class = "negative"
-      #     value = "- #{Timetrack.balance(current_user)[:value]}"
-      #   else
-      #     css_class = ""
-      #     value = Timetrack.balance(current_user)[:value]
-      #   end
-
-      #   content_tag(:b, value, :class => css_class)
-      # end
     end
-  end
-end
-
-class ActionView::Helpers::FormBuilder
-  def time_field(method, options={})
-    @template.time_field(@object_name, method, objectify_options(options))
-  end
-end
-
-
-class String
-  def roundup(base)
-    hh,mm = self.split(':')
-    hh = hh.to_i
-    mm = mm.to_i
-    mm = mm % base == 0? mm : mm + base - (mm % base)
-
-    if mm < 60
-      mm = mm > 0? mm : "00"
-      time = "#{hh}:#{mm}"
-      return time
-    else
-      return "#{hh+1}:00"
-    end
-
-    time
   end
 end
 
