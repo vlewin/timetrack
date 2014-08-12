53ea53245c141da7e26e233153727d4d1af0601c
Vladislav Lewin <vlewin@suse.de>
2014-08-12 08:34:48 +0200

    clean up timetrack controller

--- app/controllers/timetracks_controller.rb
+++ app/controllers/timetracks_controller.rb
@@ -3,34 +3,24 @@ require 'holidays/de'
 
 class TimetracksController < ApplicationController
   def index
-    @date = params[:date].nil? ? Date.today : params[:date].to_date # show current date
-    @month = (@date.beginning_of_month..@date.end_of_month).to_a # all days in month e.g. 29, 30, 31
-
-    if Timetrack.find_by_date(@date, current_user)
-      @timetrack = Timetrack.find_by_date(@date, current_user)
-    else
-      @timetrack = Timetrack.new(:date => @date, :start => Time.now)
-      @timetrack.user = current_user
-    end
-
+    @date = params[:date].nil? ? Date.today : params[:date].to_date
+    @month = (@date.beginning_of_month..@date.end_of_month).to_a
     @timetracks = Timetrack.find_by_month(@date, current_user)
+    @timetrack = current_user.timetracks.find_by_date(@date) || current_user.timetracks.new(date: @date, start: Time.now)
 
     respond_to do |format|
-      puts format.inspect
       format.html
-      format.js { render :partial => "timetrack" } # render partial if ajax call otherwise index.html
+      format.js { render partial: 'timetrack' }
     end
   end
 
   def create
-    @timetrack = Timetrack.new(timetrack_params)
-    @timetrack.user_id = current_user.id
-
+    @timetrack = current_user.timetracks.new(timetrack_params)
     respond_to do |format|
-      if @timetrack.save
-        format.html { redirect_to timetracks_path({:date => @timetrack.date}) }
+      if @timetrack.save!
+        format.html { redirect_to timetracks_path({date: @timetrack.date}) }
       else
-        format.html { redirect_to timetracks_path({:date => @timetrack.date}), :error => 'Something went wrong.' }
+        format.html { redirect_to timetracks_path({date: @timetrack.date}), error: 'Something went wrong.' }
       end
     end
   end
@@ -40,9 +30,9 @@ class TimetracksController < ApplicationController
 
     respond_to do |format|
       if @timetrack.update_attributes(timetrack_params)
-        format.html { redirect_to timetracks_path({:date => @timetrack.date}) }
+        format.html { redirect_to timetracks_path({date: @timetrack.date}) }
       else
-        format.html { redirect_to timetracks_path({:date => @timetrack.date}), :error => 'Something went wrong.'}
+        format.html { redirect_to timetracks_path({date: @timetrack.date}), error: 'Something went wrong.'}
       end
     end
   end
@@ -52,9 +42,9 @@ class TimetracksController < ApplicationController
 
     respond_to do |format|
       if @timetrack.delete
-        format.html { redirect_to timetracks_path({:date => @timetrack.date}) }
+        format.html { redirect_to timetracks_path({date: @timetrack.date}) }
       else
-        format.html { redirect_to timetracks_path({:date => @timetrack.date}), :error => 'Something went wrong.'}
+        format.html { redirect_to timetracks_path({date: @timetrack.date}), error: 'Something went wrong.'}
       end
     end
   end
