a40718359e448af315cf4169433258a33770a9fb
Vladislav Lewin <vlewin@suse.de>
2014-08-13 10:59:28 +0200

    add more helper methods to ruby classes

--- lib/extensions.rb
+++ lib/extensions.rb
@@ -1,6 +1,32 @@
+class String
+  def negative?
+    self.include?('-')
+  end
+end
+
 class Fixnum
   def negative?
-    self.to_s.match('-').present?
+    self < 0
+  end
+
+  def to_time
+    hh = (self/60.0).to_i
+    mm = (self.abs%60).to_s.rjust(2, '0')
+
+    "#{hh} h #{mm} min"
+  end
+
+  def to_minutes
+    self*60
+  end
+end
+
+class Float
+  def to_time
+    hh = (self/60.0).to_i
+    mm = (self.abs%60).to_s.rjust(2, '0')
+
+    "#{hh} h #{mm} min"
   end
 end
 
