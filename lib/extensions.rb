bc1242417786ebabd7f1166ebcb07fb28c889295
Vladislav Lewin <vlewin@suse.de>
2014-08-15 09:52:41 +0200

    rspect :long and :short format parameters

--- lib/extensions.rb
+++ lib/extensions.rb
@@ -9,12 +9,12 @@ class Fixnum
     self < 0
   end
 
-  def to_time
+  def to_time(abbr= :short)
     sign = self.negative? ? '-' : ''
     hh = (self/60.0).abs.to_i
     mm = (self.abs%60).to_s.rjust(2, '0')
 
-    "#{sign}#{hh} h #{mm} min"
+    "#{sign}#{hh} #{abbr == :short ? 'h' : 'hours'} #{mm} #{abbr == :short ? 'mm' : 'minutes'}"
   end
 
   def to_minutes
