65444eeabcb789015b28972bb16cc568fbfc743d
Vladislav Lewin <vlewin@suse.de>
2014-08-15 11:37:31 +0200

    switch to MIME responders

--- app/controllers/timetracks_controller.rb
+++ app/controllers/timetracks_controller.rb
@@ -2,16 +2,15 @@ require 'holidays'
 require 'holidays/de'
 
 class TimetracksController < ApplicationController
+  respond_to :html, :js
+
   def index
     @date = params[:date].nil? ? Date.today : params[:date].to_date
     @month = (@date.beginning_of_month..@date.end_of_month).to_a
     @timetracks = Timetrack.find_by_month(@date, current_user)
     @timetrack = current_user.timetracks.find_by_date(@date) || current_user.timetracks.new(date: @date, start: Time.now)
 
-    respond_to do |format|
-      format.html
-      format.js { render partial: 'timetrack' }
-    end
+    respond_with @timetracks
   end
 
   def create
