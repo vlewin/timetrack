d6b5981978bcebe1966d4bd5d125920196ab5850
Vladislav Lewin <vlewin@suse.de>
2014-08-15 11:12:44 +0200

    test extensions.rb

--- lib/extensions.rb
+++ lib/extensions.rb
@@ -9,26 +9,16 @@ class Fixnum
     self < 0
   end
 
-  def to_time(abbr= :short)
+  def to_time(format= :short)
     sign = self.negative? ? '-' : ''
     hh = (self/60.0).abs.to_i
     mm = (self.abs%60).to_s.rjust(2, '0')
 
-    "#{sign}#{hh} #{abbr == :short ? 'h' : 'hours'} #{mm} #{abbr == :short ? 'mm' : 'minutes'}"
+    "#{sign}#{hh} #{format == :short ? 'h' : 'hours'} #{mm} #{format == :short ? 'mm' : 'minutes'}"
   end
 
   def to_minutes
     self*60
-  end
-end
-
-class Float
-  def to_time
-    sign = self.negative? ? '-' : ''
-    hh = (self/60.0).abs.to_i
-    mm = (self.abs%60).to_s.rjust(2, '0')
-
-    "#{sign}#{hh} h #{mm} min"
   end
 end
 
---
+++ spec/lib/extensions_spec.rb
@@ -0,0 +1,50 @@
+require "rails_helper"
+
+describe 'extensions.rb' do
+  let(:positive_number) { 60 }
+  let(:negative_number) { -60 }
+
+  describe String do
+    describe '#negative?' do
+      it 'determines if a number is positive or negative' do
+        expect(negative_number.to_s.negative?).to be true
+        expect(positive_number.to_s.negative?).to be false
+      end
+    end
+  end
+
+  describe Fixnum do
+    describe '#negative?' do
+      it 'determines if a number is positive or negative' do
+        expect(negative_number.negative?).to be true
+        expect(positive_number.negative?).to be false
+      end
+    end
+
+    describe '#to_time' do
+      it 'converts minutes to time string e.g. 60.to_time => "1 h 00 mm"' do
+        expect(positive_number.to_time).to eq  '1 h 00 mm'
+        expect(negative_number.to_time).to eq '-1 h 00 mm'
+      end
+
+      it 'dafaults to :short format' do
+        expect(positive_number.to_time).to eq '1 h 00 mm'
+      end
+
+      it 'respects format parameter' do
+        expect(positive_number.to_time(:short)).to eq '1 h 00 mm'
+        expect(positive_number.to_time(:long)).to eq '1 hours 00 minutes'
+      end
+    end
+  end
+end
+
+
+describe Date do
+  describe '#weekend?' do
+    it 'determines if a date falls on the weekend' do
+      expect(Date.parse('2014-08-16').weekend?).to be true
+      expect(Date.parse('2014-08-15').weekend?).to be false
+    end
+  end
+end
