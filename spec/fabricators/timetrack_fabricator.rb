77ff1175b7b360b8035d2db35ae24ff2a3448de2
Vladislav Lewin <vlewin@suse.de>
2014-08-15 15:18:39 +0200

    improve timetrack fabrication start and finish attributes

--- spec/fabricators/timetrack_fabricator.rb
+++ spec/fabricators/timetrack_fabricator.rb
@@ -1,7 +1,7 @@
 Fabricator(:timetrack) do
   date { sequence(:date) { |i| i.days.ago } }
-  start { Time.now }
-  finish { 8.hours.from_now }
+  start { Time.at('8:00'.to_time) }
+  finish { Time.at('17:00'.to_time) }
 
   user
 end
