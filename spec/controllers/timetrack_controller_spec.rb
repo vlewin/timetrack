5a76c08e70f004dfe13baa071fa40ee5dca6cc02
Vladislav Lewin <vlewin@suse.de>
2014-08-15 15:19:36 +0200

    add more controller tests

--- spec/controllers/timetrack_controller_spec.rb
+++ spec/controllers/timetrack_controller_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
 describe TimetracksController, type: :controller do
-  let(:user) {Fabricate(:user)}
+  let(:user) { Fabricate(:user) }
+  let(:timetrack) { user.timetracks.last }
+  let(:new_timetrack) { Fabricate.build(:timetrack, date: '2000-01-01') }
 
   before do
     sign_in user
@@ -34,4 +36,33 @@ describe TimetracksController, type: :controller do
       expect(assigns(:timetracks)).to match_array(user.timetracks)
     end
   end
+
+  describe 'POST #create' do
+    it 'saves the new timetrack in the database and redirects to the index page' do
+      timetracks_count = user.timetracks.count
+
+      post :create, { timetrack: new_timetrack.attributes }
+
+      expect(user.timetracks.count).to eq(timetracks_count+1)
+      expect(response).to redirect_to(timetracks_path(date: new_timetrack.date))
+    end
+
+    it 'redirects to the index page with an error if save fails' do
+    end
+  end
+
+  describe 'PUT/PATCH #update' do
+    it 'updates the existing timetrack and redirects to the index page' do
+      finish = timetrack.finish.to_time + 10.minutes
+
+      put :update, id: timetrack, timetrack: { finish: finish }
+
+      expect(timetrack.reload.finish).to eq(finish)
+      expect(response).to redirect_to(timetracks_path(date: timetrack.date))
+    end
+
+    it 'redirects to the index page with an error if update fails' do
+    end
+  end
+
 end
