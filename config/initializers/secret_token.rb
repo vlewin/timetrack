c83e70498267004e4a49abc161fb99eece65e129
Vladislav Lewin <vlewin@users.noreply.github.com>
2017-10-09 12:35:32 +0200

    Update secret_token.rb

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Timestamps::Application.config.secret_token = = ENV['SECRET_KEY_BASE'] || '49f8e2e1ce765cc4a310f1ce8108a260a42987da7dd2b3406897bb37906e73e113fa452eef8f1489f8de8eccb9954ca9b359c8124c255ac3aac367af3418cb04'
+Timestamps::Application.config.secret_token = ENV['SECRET_KEY_BASE'] || '49f8e2e1ce765cc4a310f1ce8108a260a42987da7dd2b3406897bb37906e73e113fa452eef8f1489f8de8eccb9954ca9b359c8124c255ac3aac367af3418cb04'
