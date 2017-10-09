92f45ab9df4902f5856751035466042a96299e28
Vladislav Lewin <vlewin@users.noreply.github.com>
2017-10-09 12:39:23 +0200

    Update devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,11 +1,12 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+  config.secret_key = ENV['SECRET_KEY_BASE'] || Rails.application.secrets['secret_key_base']
+
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
-  config.secret_key = ENV['SECRET_KEY_BASE'] || 'very-secure-string'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
